b7e8f5b3eb88eb8c58de7f61e3573d2a3e1e4dbb
saglin <saglin-2.student.ltu.se>
2025-02-11 16:45:46 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+from flask import Flask, render_template, jsonify
+from flask_mysqldb import MySQL
+import random
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'databas'
+mysql = MySQL(app)
+
+@app.route("/browse")
+def index():
+    cursor = mysql.connection.cursor()
+    cursor.execute('''SELECT * FROM Item''')
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template("index.html", items=data)
+
+@app.route("/item/<int:id>")
+def item_page(id):
+    cursor = mysql.connection.cursor()
+    cursor.execute('''SELECT * FROM Item WHERE Item.id=%s''', (id,))
+    data = cursor.fetchone()
+    cursor.close()
+    return render_template("item.html", item=data[1], price=data[2], stock=data[3], description=data[4])
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+
+# Check if the username and password are legitimate
+def check_credentials():
+    return
+
+app.run(host="0.0.0.0", port=80)
